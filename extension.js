d42267390628ceb31e7b7786d51b4928cdf3a19b
Prajwal Gowda GN <prajwal.gowda.gn@autodesk.com>
2024-03-06 12:08:17 +0530

    refactored (#1)
    ---------------
    1. Added command to take the stack trace from the terminal
    2. Added command to fetch the data from the window

--- extension.js
+++ extension.js
@@ -6,6 +6,7 @@ const vscode = require('vscode');
  */
 function activate(context) {
 	createStatusBarItem(context);
+    getActiveEditorData(context);
 
 	console.log('Congratulations, your extension "codehealer" is now active!');
 	let disposable = vscode.commands.registerCommand('codehealer.heal', function () {
@@ -15,17 +16,45 @@ function activate(context) {
 	context.subscriptions.push(disposable);
 }
 
+function getActiveEditorData(context){
+
+    const getActiveEditorDataId = 'codehealer.data';
+    context.subscriptions.push(vscode.commands.registerCommand(getActiveEditorDataId, async () => 
+    {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+        const document = editor.document;
+        const text = document.getText();
+        console.log(text);
+        vscode.commands.executeCommand("workbench.action.revertAndCloseActiveEditor");
+        return text;
+    }
+    else{
+        vscode.window.showInformationMessage('No active editor found');
+    }
+    }));
+
+    const item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+    item.command = getActiveEditorDataId;
+
+    context.subscriptions.push(item);
+
+    item.text = `GetActiveEditorData`;
+    item.tooltip = `active editor data tooltip`;
+    item.show();
+}
+
 function createStatusBarItem(context)
 {
     // register a command that is invoked when the status bar
     // item is clicked.
-    const myCommandId = 'myExtension.statusBarClick';
+    const myCommandId = 'codehealer.statusBarClick';
     context.subscriptions.push(vscode.commands.registerCommand(myCommandId, async () => 
     {
-        const pageType = await vscode.window.showQuickPick(
-            ['shell', 'fetch rows, list in table'],
-            { placeHolder: 'select type of web page to make' });
-
+        // const pageType = await vscode.window.showQuickPick(
+        //     ['shell', 'fetch rows, list in table'],
+        //     { placeHolder: 'select type of web page to make' });
+        vscode.commands.executeCommand("extension.terminalCapture.runCapture")
     }));
 
     // create a new status bar item that we can now manage
